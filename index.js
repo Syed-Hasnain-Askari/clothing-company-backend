0783989b8404295c5e7f5519ad585feb92cc84b2
Syed-Hasnain-Askari <hasnainaskari32@gmail.com>
2023-03-24 04:34:54 +0500

    add files

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+var mongoose = require('mongoose');
+var app = require('./app');
+var config = require('./configuration/config');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const http = require('http');
+var port = 3977;
+mongoose.connect('mongodb://' + config.mongo.uri + ':27017/' + config.mongo.db)
+  .then(() => {
+    app.use(bodyParser.json());
+    //Enable CORS policy
+    app.use(cors());
+    app.options('*', cors());
+    var server = http
+      .createServer(app)
+      .listen(
+        port,
+        console.log(`Server is running on the port no: ${port} `),
+      );
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+  });
