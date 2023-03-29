a8f43ed8b879c5706f983ae3d84229ff74a121c1
Syed-Hasnain-Askari <hasnainaskari32@gmail.com>
2023-03-30 04:24:31 +0500

    approved budget now can update the budget in employeeproducts

--- controller/budgetRequest.js
+++ controller/budgetRequest.js
@@ -1,4 +1,5 @@
 const budgetRequest = require("../models/budgetRequest")
+const budget = require("../models/employeeProducts")
 var mongoose = require('mongoose');
 const ObjectId = mongoose.Types.ObjectId;
 const addRequest = async (req, res) => {
@@ -27,17 +28,25 @@ const addRequest = async (req, res) => {
 }
 const approvedRequest = async (req, res) => {
     // Access values in req.body
-    const { employeeId, approvedAmount } = req.body;
+    const { employeeId, approvedAmount,productId } = req.body;
     console.log(req.body)
     try {
+         // Update the approvedAmount and status values in request collection
         const updatedRequest = await budgetRequest.findOneAndUpdate(
             { employeeId: employeeId },
             { $set: { approvedAmount: approvedAmount, status: 1 } },
             { new: true }
         );
+           // Update the budget value from employeeProducts collection
+           const updatedBudget = await budget.findOneAndUpdate(
+            { _id: productId},
+            { $inc: { budget: approvedAmount } },
+            { new: true }
+        );
         res.status(200).json({
-            response:updatedRequest,
-            message:"Request has been approved"
+            updatedRequest: updatedRequest,
+            updatedBudget: updatedBudget,
+            message: 'Request has been approved'
         });
     } catch (error) {
         console.log(error);
