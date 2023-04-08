7c9a2d206a111e13d88fe6008ddeec35327e04b9
subhan300 <subhan.akram2400@gmail.com>
2023-04-09 00:18:21 +0500

    connection to database added

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const http = require('http');
 var port = 3977;
-mongoose.connect('mongodb://0.0.0.0:27017/clothingcompany')
-  .then(() => {
+const DB =
+  "mongodb+srv://clothingcompany:clothingcompany123@cluster0.61kmc.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(DB,{
+    useNewUrlParser: true, useUnifiedTopology: true 
+}).then(()=>{
+    console.log("connection successfully !")
     app.use(bodyParser.json());
     //Enable CORS policy
     app.use(cors());
@@ -21,7 +25,23 @@ mongoose.connect('mongodb://0.0.0.0:27017/clothingcompany')
         port,
         console.log(`Server is running on the port no: ${port} `),
       );
-  })
-  .catch((err) => {
-    console.error('Failed to connect to MongoDB', err);
-  });
+}).catch((err)=>{console.log("err",err)})
+// mongoose.connect('mongodb://0.0.0.0:27017/clothingcompany')
+//   .then(() => {
+//     app.use(bodyParser.json());
+//     //Enable CORS policy
+//     app.use(cors());
+//     app.options('*', cors());
+//     app.use(cors());
+//     app.use(bodyParser.urlencoded({ extended: false }));
+//     app.use(bodyParser.json());
+//     http
+//       .createServer(app)
+//       .listen(
+//         port,
+//         console.log(`Server is running on the port no: ${port} `),
+//       );
+//   })
+//   .catch((err) => {
+//     console.error('Failed to connect to MongoDB', err);
+//   });
