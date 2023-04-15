0f171c9dcb02f562df7ef537f62852bb820eb00d
Syed-Hasnain-Askari <hasnainaskari32@gmail.com>
2023-04-15 05:24:42 +0500

    modify budget request

--- controller/budgetRequest.js
+++ controller/budgetRequest.js
@@ -4,24 +4,23 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Types.ObjectId;
 const getBudgetRequest = async (req, res) => {
   try {
-    const getBudgetRequest = await BudgetRequest.aggregate(
-      [
-        {
-          $lookup: {
-            from: 'employees',
-            localField: 'employeeId',
-            foreignField: '_id',
-            as: 'result'
-          }
-        }, {
-          $project: {
-            'result.employeePassword': 0,
-            'result.companyName': 0,
-            'result.productsId': 0
-          }
-        }
-      ]
-    );
+    const getBudgetRequest = await BudgetRequest.aggregate([
+      {
+        $lookup: {
+          from: 'employees',
+          localField: 'employeeId',
+          foreignField: '_id',
+          as: 'result',
+        },
+      },
+      {
+        $project: {
+          'result.employeePassword': 0,
+          'result.companyName': 0,
+          'result.productsId': 0,
+        },
+      },
+    ]);
     res.status(200).send(getBudgetRequest);
   } catch (error) {
     res.send('Something went wrong').status(500);
@@ -32,32 +31,31 @@ const addRequest = async (req, res) => {
   const { employeeId, requestAmount } = req.body;
   try {
     BudgetRequest.find({
-      employeeId: { $in: employeeId }
-    })
-      .then(async (existingEmployees) => {
-        if (existingEmployees.length > 0) {
-          return res.status(400).send({
-            message: 'Sorry! You have already sent your budget request'
-          });
-        } else {
-          const newRequest = new BudgetRequest({
-            employeeId: new ObjectId(employeeId),
-            requestAmount,
-            approvedAmount: 0,
-            status: 0
-          });
-          const requestCreated = await newRequest.save();
-          res.status(200).send({
-            result: requestCreated,
-            message: 'Request has been created successfully!'
-          });
-        }
-      });
+      employeeId: { $in: employeeId },
+    }).then(async (existingEmployees) => {
+      if (existingEmployees.length > 0) {
+        return res.status(400).send({
+          message: 'Sorry! You have already sent your budget request',
+        });
+      } else {
+        const newRequest = new BudgetRequest({
+          employeeId: new ObjectId(employeeId),
+          requestAmount,
+          approvedAmount: 0,
+          status: 0,
+        });
+        const requestCreated = await newRequest.save();
+        res.status(200).send({
+          result: requestCreated,
+          message: 'Request has been created successfully!',
+        });
+      }
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send({
       message: 'Something went wrong!',
-      error
+      error,
     });
   }
 };
@@ -65,28 +63,32 @@ const approvedRequest = async (req, res) => {
   // Access values in req.body
   const { employeeId, approvedAmount, status, requestId } = req.body;
   try {
-    // Update the approvedAmount and status values in request collection
-    const updatedRequest = await BudgetRequest.findOneAndUpdate(
-      { employeeId },
-      { $set: { approvedAmount, status } },
-      { new: true }
-    );
-    // Update the budget value from employeeProducts collection
-    const updatedBudget = await employee.findOneAndUpdate(
-      { _id: employeeId },
-      { $inc: { budget: approvedAmount } },
-      { new: true }
-    );
-    const removedRequest = await BudgetRequest.findByIdAndRemove(requestId);
-    if (!removedRequest) {
-      res.status(400).send({ message: 'ID not found' });
+    if (status === 2) {
+      const removedRequest = await BudgetRequest.findByIdAndRemove(requestId);
+      return res.status(401).json({
+        message: 'Your request has been rejected',
+        removedRequest,
+      });
     } else {
-      // eslint-disable-next-line max-len
-      const message = status === 1 ? 'Your request has been accepted' : 'Your request has not been rejected';
-      res.status(200).json({
+      // Update the approvedAmount and status values in request collection
+      const updatedRequest = await BudgetRequest.findOneAndUpdate(
+        { employeeId },
+        { $set: { approvedAmount, status } },
+        { new: true },
+      );
+      // Update the budget value from employeeProducts collection
+      const updatedBudget = await employee.findOneAndUpdate(
+        { _id: employeeId },
+        { $inc: { budget: approvedAmount } },
+        { new: true },
+      );
+      const removedRequest = await BudgetRequest.findByIdAndRemove(requestId);
+      const message = 'Your request has been accepted';
+      return res.status(200).json({
+        removedRequest,
         updatedRequest,
         updatedBudget,
-        message
+        message,
       });
     }
   } catch (error) {
@@ -103,11 +105,11 @@ const changeBudgetByManager = async (req, res) => {
     const updatedBudget = await employee.findOneAndUpdate(
       { _id: employeeId },
       { $set: { budget: changeBudgetAmount } },
-      { new: true }
+      { new: true },
     );
     res.status(200).json({
       updatedBudget,
-      message: 'Employee budget has been changed'
+      message: 'Employee budget has been changed',
     });
   } catch (error) {
     console.log(error);
@@ -118,5 +120,5 @@ module.exports = {
   getBudgetRequest,
   addRequest,
   approvedRequest,
-  changeBudgetByManager
+  changeBudgetByManager,
 };
