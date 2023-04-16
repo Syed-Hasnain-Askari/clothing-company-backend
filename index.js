c4e4b254a620e4ad9f826f586f9686e42d75a434
Syed-Hasnain-Askari <hasnainaskari32@gmail.com>
2023-04-17 01:53:47 +0500

    change connection string

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ const port = 3977;
 // mongodb://0.0.0.0:27017/clothingcompany
 // useNewUrlParser: true, useUnifiedTopology: true
 const DB = Config.mongo.uri;
-mongoose.connect("mongodb://0.0.0.0:27017/clothingcompany", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-  console.log('connection successfully !');
-  app.use(bodyParser.json());
-  // Enable CORS policy
-  app.use(cors());
-  app.options('*', cors());
-  app.use(cors());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
-  http
-    .createServer(app)
-    .listen(
-      process.env.PORT || { port },
-      console.log(`Server is running on the port no: ${port} `)
-    );
-}).catch((err) => { console.log('err', err); });
+mongoose
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connection successfully !');
+    app.use(bodyParser.json());
+    // Enable CORS policy
+    app.use(cors());
+    app.options('*', cors());
+    app.use(cors());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    http.createServer(app).listen(process.env.PORT || { port }, console.log(`Server is running on the port no: ${port} `));
+  })
+  .catch((err) => {
+    console.log('err', err);
+  });
